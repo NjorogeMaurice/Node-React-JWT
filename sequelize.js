f6ea892731181290fc5875963ddb31403f134238
NjorogeMaurice <njoroge0330@gmail.com>
2024-03-06 10:03:04 +0300

    update .env

--- sequelize.js
+++ sequelize.js
@@ -1,9 +1,11 @@
 // sequelize.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('example', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.SQL_DB_NAME, process.env.SQL_USER_NAME, process.env.SQL_USER_PASSWORD, {
+  host: process.env.SQL_DB_HOST,
+  dialect: process.env.SQL_DB_DIALECT
 });
+
+
 
 module.exports = sequelize;
